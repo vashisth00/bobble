550e9d5ad0f71f028d9fcd7863bee347bbcdc135
Vashisth00 <vashisthbhushan@gmail.com>
2020-09-06 17:36:57 +0530

    Changes

--- src/signup/main.js
+++ src/signup/main.js
@@ -1,43 +1,47 @@
 import React from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import '../index.css'
 
 function SignUp() {
   return (
-    <div className="App">
- <div className="form-wrap">
-          <div className="tabs">
-            <h3 className="signup-tab"><a className="active" href="#signup-tab-content">Sign Up</a></h3>
-            <h3 className="login-tab"><a href="#login-tab-content">Login</a></h3>
-          </div>{/*.tabs*/}
-          <div className="tabs-content">
-            <div id="signup-tab-content" className="active">
-              <form className="signup-form" action method="post">
-                <input type="email" className="input" id="user_email" autoComplete="off" placeholder="Email" />
-                <input type="text" className="input" id="user_name" autoComplete="off" placeholder="Username" />
-                <input type="password" className="input" id="user_pass" autoComplete="off" placeholder="Password" />
-                <input type="submit" className="button" defaultValue="Sign Up" />
-              </form>{/*.login-form*/}
-              <div className="help-text">
-                <p>By signing up, you agree to our</p>
-                <p><a href="#">Terms of service</a></p>
-              </div>{/*.help-text*/}
-            </div>{/*.signup-tab-content*/}
-            <div id="login-tab-content">
-              <form className="login-form" action method="post">
-                <input type="text" className="input" id="user_login" autoComplete="off" placeholder="Email or Username" />
-                <input type="password" className="input" id="user_pass" autoComplete="off" placeholder="Password" />
-                <input type="checkbox" className="checkbox" id="remember_me" />
-                <label htmlFor="remember_me">Remember me</label>
-                <input type="submit" className="button" defaultValue="Login" />
-              </form>{/*.login-form*/}
-              <div className="help-text">
-                <p><a href="#">Forget your password?</a></p>
-              </div>{/*.help-text*/}
-            </div>{/*.login-tab-content*/}
-          </div>{/*.tabs-content*/}
-        </div>
+    <div>
+    <div className="brand-logo">
+      Your Brand Logo Here
     </div>
+    <div className="form-wrap">
+      <div className="tabs-content">
+        <div id="signup-tab-content" className="active">
+          <div>
+            <div className="signup">Sign Up</div>
+            <div className="create">Create Your Account</div>
+            <div className="top">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</div>
+          </div>
+          <div className="logos">
+            <img src="../../public/fb.png" className="logo" />
+            <img src="../../public/google.png" className="logo" />
+          </div>
+          <div className="padding-line">
+            <div className="line-sep">
+              <span className="line-sep2">
+                or 
+              </span>
+            </div>
+          </div>
+          <form className="signup-form" action method="post">
+            <input type="email" className="input" id="user_email" autoComplete="off" placeholder="First Name" />
+            <input type="text" className="input" id="user_name" autoComplete="off" placeholder="Last Name" />
+            <input type="email" className="input" id="user_email" autoComplete="off" placeholder="Email Address" />
+            <input type="password" className="input" id="user_pass" autoComplete="off" placeholder="Password" />
+            <div className="help-text">
+              <p>By clicking signing up, you agree to our <a href="#">Terms of Use</a> and our <a>Privacy Policy</a></p>
+            </div>{/*.help-text*/}
+            <input type="submit" className="button" defaultValue="Sign Up" />
+          </form>{/*.login-form*/}
+        </div>{/*.signup-tab-content*/}
+      </div>{/*.tabs-content*/}
+    </div>{/*.form-wrap*/}
+  </div>
   );
 }
 
---
+++ src/signup/sucess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    console.log('Logged in');
+
+  return (
+      
+    <div className="App">
+      <p>Success</p>
+    </div>
+    
+  );
+  
+}
+
+export default App;
